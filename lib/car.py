754c134e38ad431de13b7d878d1e18e70a1ced9e
Diana Chepkoech <dianachepkoech336@gmail.com>
2023-12-07 22:41:58 +0300

    completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,5 +1,6 @@
-from lib.vehicle import Vehicle
+from vehicle import Vehicle
 
 class Car(Vehicle):
     def go(self):
         return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    pass
--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,5 +1,9 @@
-import sys
-import os
+#!/usr/bin/env python3
 
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+def pytest_itemcollected(item):
+    par = item.parent.obj
+    node = item.obj
+    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
+    suf = node.__doc__.strip() if node.__doc__ else node.__name__
+    if pref or suf:
+        item._nodeid = ' '.join((pref, suf))
